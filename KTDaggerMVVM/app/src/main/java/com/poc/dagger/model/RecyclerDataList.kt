41a5b7769b29da019a078a81a25cd8c6283ac4dc
Stalongiles Philip <stalon.giles@gmail.com>
2025-11-04 00:03:07 +0530

    Delete KTDaggerMVVM directory

--- KTDaggerMVVM/app/src/main/java/com/poc/dagger/model/RecyclerDataList.kt
+++
@@ -1,8 +0,0 @@
-package com.poc.dagger.model
-
-import dagger.Module
-
-@Module
-data class RecyclerDataList(val items:List<RecyclerData>)
-data class RecyclerData(val name:String?,val description:String?,val owner:Owner?)
-data class Owner(val avatarURL: String?)
